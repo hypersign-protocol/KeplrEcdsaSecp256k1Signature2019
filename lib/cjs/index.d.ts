f3d9d762113a82ed78f2120dbe2f0e273dbd9140
Pratap2018 <pratapmridha@gmail.com>
2024-04-16 17:52:20 +0530

    feat: expose documentLoader

--- lib/cjs/index.d.ts
+++ lib/cjs/index.d.ts
@@ -1,2 +1,3 @@
 import { EcdsaSecp256k1Signature2019 } from "./EcdsaSecp256k1Signature2019/EcdsaSecp256k1Signature2019";
-export { EcdsaSecp256k1Signature2019 };
+import { docloader, CONTEXTS } from "./Context/v1";
+export { EcdsaSecp256k1Signature2019, docloader, CONTEXTS };
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 // @ts-ignore
 import { EcdsaSecp256k1Signature2019 } from "./EcdsaSecp256k1Signature2019/EcdsaSecp256k1Signature2019";
 
+import { docloader ,CONTEXTS} from "./Context/v1";
 
-
-export { EcdsaSecp256k1Signature2019 };
+export { EcdsaSecp256k1Signature2019, docloader,CONTEXTS };
